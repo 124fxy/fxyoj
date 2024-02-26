6f457225f5acaf50cc5ade10e0e902ffa6a05638
fxy <334645fxy@163.com>
2024-02-26 18:15:50 +0800

    first commit

---
+++ src/main/java/com/yupi/fxyoj/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.yupi.fxyoj.exception;
+
+import com.yupi.fxyoj.common.BaseResponse;
+import com.yupi.fxyoj.common.ErrorCode;
+import com.yupi.fxyoj.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestControllerAdvice
+@Slf4j
+public class  GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
